a167dccfbd5ff580f625b501378e3ae4d746c2a5
Vyankatesh Poojari <65769298+vyankatesh96@users.noreply.github.com>
2025-01-12 11:55:28 +0530

    initial-structure

---
+++ src/main/java/com/kaaryapro/KaaryaproApplication.java
@@ -0,0 +1,13 @@
+package com.kaaryapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaaryaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaaryaproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaaryapro/KaaryaproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaaryapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaaryaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
